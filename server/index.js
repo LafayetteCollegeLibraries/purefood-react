e9e828dac82196d52dd07c87c0d1e9c0cf79f0b2
adam malantonio <malantoa@lafayette.edu>
2017-11-14 16:45:46 -0500

    add build step + remove test deps (for now)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('*', function (req, res) {
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './build/index.js',
 
   output: {
     filename: 'bundle.js',
